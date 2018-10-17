0a1c3abd8ee346cf2e64e5ba1a3d82a5c8231597
Nis Meinert <nis.meinert@gmail.com>
2018-10-17 10:34:59 +0200

    add possibility to change default port

--- hepana-notebook.sh
+++ hepana-notebook.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm -it -p 8888:8888 -v "$PWD":/home/jovyan/work avitase/hepana-notebook:latest
+if [ $# -eq 1 ]; then
+    port=$1
+else
+    port=8888
+fi
+echo "Starting notebook at 'localhost:$port' ..."
+
+docker run --rm -it -p $port:8888 -v "$PWD":/home/jovyan/work avitase/hepana-notebook:latest
