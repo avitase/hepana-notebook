1d23f601a1b1e5b537e14f76bb304962cb69f9a6
Nis Meinert <nis.meinert@gmail.com>
2018-10-19 15:55:59 +0200

    bugfix for k=0

--- scripts/histdump.py
+++ scripts/histdump.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 def poisson_interval(k, alpha=2.*0.159):
-    if k == 0: return -math.log(alpha)
+    if k == 0: return 0., -math.log(alpha)
     high = chi2.ppf(1.-alpha/2., 2*k + 2) / 2.
     low = chi2.ppf(alpha/2., 2*k) / 2. if k > 0 else 0.
     return low, high
